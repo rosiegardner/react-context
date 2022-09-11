1521293a49922dae5a1388fa0d187d8f3ed9db51
Rosie Gardner <rosiegardner78@gmail.com>
2022-09-11 15:49:53 -0700

    import new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import ToggleTheme from "./ToggleTheme";
 
 function App(){
   return (
     <React.Fragment>
       <Header />
+      <ToggleTheme />
       <TicketControl />
     </React.Fragment>
   );
